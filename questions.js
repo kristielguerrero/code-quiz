2c8c50a94e1566f59e200e43a15779b6fdbc2a7d
Kris Tina <kristyguerrero20@gmail.com>
2021-05-30 23:34:35 -0500

    fixed the timer, added two more questions, created functions to end the quiz and created and linked the style.css folder to hide some button elements

--- questions.js
+++ questions.js
@@ -1,7 +1,17 @@
-var question = [
+var questions = [
   {
     title: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    title: "Question2:",
+    choices: ["one", "two", "three", "four"],
+    answer: "alerts",
+  },
+  {
+    title: "Question 3:",
+    choices: ["purplle", "green", "orange", "black"],
     answer: "alerts",
   },
 ];
--- script.js
+++ script.js
@@ -2,14 +2,19 @@ console.log(questions);
 
 var time = questions.length * 10;
 var timer = null;
-
+var currentQuestion;
+var questionEl = document.getElementById("title");
 var qIndex = 0;
-
+var endGameEl = document.getElementById("end-game");
+var choicesEl = document.getElementById("choices");
 var startBtn = document.getElementById("start-btn");
 var timeEL = document.getElementById("time");
 //event listeners
 
 function startQuiz() {
+  console.log("insideStartQuiz");
+  startBtn.style.display = "none";
+  timeEL.textContent = time;
   timer = setInterval(function () {
     time--;
     timeEL.textContent = time;
@@ -24,15 +29,21 @@ function endQuiz() {
   clearInterval(timer);
 }
 
+function endGame() {
+  questionEl.style.display = "none";
+  endGameEl.style.display = "block";
+}
+
 function showQuestions() {
-  var currentQuestion = questions[qIndex];
+  currentQuestion = questions[qIndex];
   console.log(currentQuestion);
   questionEl.textContent = currentQuestion.title;
   choicesEl.innerHTML = "";
 
   for (var i = 0; i < currentQuestion.choices.length; i++) {
     var newBtn = document.createElement("button");
-    newBtn.textContent = choices;
+    console.log(currentQuestion.choices[i]);
+    newBtn.textContent = currentQuestion.choices[i];
     choicesEl.appendChild(newBtn);
     newBtn.onclick = choiceClick;
   }
@@ -47,8 +58,13 @@ function choiceClick(event) {
     console.log("You got it wrong");
     time -= 5;
   }
-  qIndex++;
-  showQuestions();
+  //   qIndex++;
+  if (qIndex === questions.length - 1) {
+    endGame();
+  } else {
+    qIndex++;
+    showQuestions();
+  }
 }
 
 startBtn.onclick = startQuiz;
